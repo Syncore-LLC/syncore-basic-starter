a8fc06bb8cb5e2de6816637c08b692358f7142bc
Shaun Chander <shaunchander@gmail.com>
2020-10-26 16:39:09 -0500

    ✨ init commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./**/*.html', '/*.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
